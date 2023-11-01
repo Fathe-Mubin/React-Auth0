b1820b600245cbc623d172ce90f258bf135af471
Fathe Mubin <fathe.mubin@bjitgroup.com>
2023-11-01 15:32:49 +0600

    [React Auth SPA]
    - Create a new react app
    - connected with Auth0
    - Implemented login and logout

---
+++ src/components/Profile.js
@@ -0,0 +1,22 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+export const Profile = () => {
+  const { user, isAuthenticated } = useAuth0();
+  return (
+    isAuthenticated && (
+      <article className="column">
+        {user?.picture && <img src={user?.picture} alt={user?.name} height='87' width='200' />}
+        <h2>{user.name}</h2>
+        <ul>
+          {Object.keys(user).map((objKey, i) => (
+            <li key={i}>
+              {objKey}: {user[objKey]}
+            </li>
+          ))}
+        </ul>
+      </article>
+    )
+  );
+};
+
+export default Profile;
